d41013d995d529eeac22fecded85edd929cbe523
binu <binualexander05@gmail.com>
2020-09-01 19:26:29 +0100

    initial commit

---
+++ HelloWorldService/src/main/java/com/skill/test/TestService.java
@@ -0,0 +1,12 @@
+package com.skill.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestService {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestService.class, args);
+	}
+}
